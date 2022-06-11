c4eb5d550429c90d0f503aa40529f5eca4901673
gamel <simon@pop-os.localdomain>
2022-06-11 17:52:55 +0200

    added tasks to docs

---
+++ docs/tasks.md
@@ -0,0 +1,33 @@
+
+# Tasks
+
+## Project Management
+
+* project architecture overview
+* GUI screens and screen sequence sketches
+
+## Core Application
+
+### Graphical User Interface
+
+* custom graphics
+* animations
+
+### Controller
+
+* intuitive mouse controls and keyboard shortcuts
+
+### Model
+
+* formalize model in trait 
+
+## Further Extensions
+
+* level generator (different strategies possible)
+* puzzle solver (different strategies possible)
+* import levels from file with parser
+* export levels to file (persistence)
+* game and level statistics
+* level editor
+* game server to manage level data and level + user statistics
+* audio support
--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,16 @@ extern crate quickcheck_macros;
 
 mod model;
 
-use model::board::*;
+use model::grid::*;
+use model::testlevel::*;
 use model::tile::*;
-use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-
-    let mut rng = rand::thread_rng();
-
-    let rows = 10;
-    let columns = 10;
-
-    let random_vals = (0..rows * columns)
-        .map(|_| Tile::new(rng.gen_range(0..16)))
-        .collect();
-
-    let mut board = Board::new(rows, columns, random_vals);
-    println!("{board}");
-    println!("{}", "-".repeat(rows));
-    board = board.map(|t| t.rotate_clockwise(1));
-    println!("{}", board)
+    let levels: Vec<Grid<Tile>> = TEST_LEVELS
+        .map(|l| parse_level(l, char_to_tile).unwrap())
+        .to_vec();
+    levels
+        .iter()
+        .zip(1..)
+        .for_each(|(l, i)| println!("level {i}\n{l}\n"));
 }
