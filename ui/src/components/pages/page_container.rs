07a6312382bd6b0b65e78fb482d9c70bd929f9ec
Jan Alexander Jensen <a.jensen@campus.lmu.de>
2022-08-28 16:41:03 +0200

    fix head message hidden too soon on multiple trigger

--- ui/src/components/pages/page_container.rs
+++ ui/src/components/pages/page_container.rs
@@ -14,6 +14,8 @@ use game::model::coordinate::Coordinate;
 #[function_component(PageContainer)]
 pub fn page_container() -> Html {
     let head_message = use_state_eq(|| "".to_string());
+    let head_message_timeout_id = use_state(|| -1_i32);
+
     let dimension = use_state(|| Coordinate::new(5 as usize, 5 as usize));
     let level_number = use_state(|| 0);
     let screen = use_state(|| Screen::Title);
@@ -64,11 +66,15 @@ pub fn page_container() -> Html {
     // depends on message change
     {
         let head_message = head_message.clone();
+        let timeout_id = head_message_timeout_id.clone();
         use_effect_with_deps(
             move |message| {
+                let window = web_sys::window().unwrap();
                 let message_string = (*message.clone()).clone();
                 if message_string != "".to_string() {
-                    let window = web_sys::window().unwrap();
+                    if *timeout_id != -1 {
+                        window.clear_timeout_with_handle(*timeout_id);
+                    }
                     let document = window.document().unwrap();
                     let msg_element = document.get_element_by_id("head-message").unwrap();
                     msg_element.remove_attribute("hidden").ok();
@@ -76,23 +82,26 @@ pub fn page_container() -> Html {
                     let hide_action = {
                         let message = message.clone();
                         let msg_element = msg_element.clone();
+                        let timeout_id = timeout_id.clone();
                         Closure::<dyn Fn()>::new(move || {
                             msg_element.set_attribute("hidden", "true").ok();
                             message.set("".to_string());
+                            timeout_id.set(-1);
                         })
                     };
 
-                    window
+                    let id = window
                         .set_timeout_with_callback_and_timeout_and_arguments_0(
                             hide_action.as_ref().unchecked_ref(),
-                            1500,
+                            2000,
                         )
-                        .ok();
-
+                        .ok().unwrap();
+            
+                    timeout_id.set(id);
                     hide_action.forget();
                 }
 
-                || ()
+                || {}
             },
             head_message,
         )
