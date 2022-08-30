e229840184a44870110f18b3a0a01704cff67936
Jan Alexander Jensen <a.jensen@campus.lmu.de>
2022-08-30 13:02:30 +0200

    modify default logging level to info

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -6,34 +6,12 @@ use yew::prelude::*;
 
 use crate::components::pages::page_container::PageContainer;
 
-
-use game::model::fastgen::generate;
-use game::model::coordinate::Coordinate;
-use crate::components::map::board::BoardComponent;
-use crate::helper::screen::Screen;
-
 #[function_component(App)]
 fn app() -> Html {
-
-    let level_grid = generate(Coordinate {row : 5, column: 5}, 99);
-    let screen = use_state(|| Screen::Title);
-    let msg = use_state(|| "".to_string());
-
     html! {
         <>
             <div id="title">{"Rusty infinity loop!"}</div>
-            
-            // <PageContainer />
-            <div id="container">
-                <BoardComponent 
-                    level_grid={level_grid}
-                    screen={screen.clone()}
-                    message={msg.clone()}
-                >
-                </BoardComponent>
-            </div>
-
-
+            <PageContainer />
             <div id="footer">
                 <a href={"https://uni2work.ifi.lmu.de/course/S22/IfI/Rust"}>
                     {"High level languages: Rust"}
@@ -45,6 +23,6 @@ fn app() -> Html {
 }
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
     yew::start_app::<App>();
 }
