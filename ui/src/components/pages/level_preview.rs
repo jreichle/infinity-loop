c7708243d1afe49ba36cc01d5422dbeaaa9c33a7
Johannes Reichle <jreichle@hm.edu>
2022-09-02 12:55:02 +0200

    Enable saving loaded nr oflevels for preview

--- ui/src/components/pages/level_preview.rs
+++ ui/src/components/pages/level_preview.rs
@@ -6,7 +6,9 @@ use rand::Rng;
 use crate::components::board::level::StatelessLevelComponent;
 use crate::components::reducers::preview_reducer::{PreviewAction, PreviewState};
 
-use crate::helper::local_storage::{change_screen, retrieve_editor_level};
+use crate::helper::local_storage::{
+    change_screen, retrieve_editor_level, retrieve_preview_level_count, save_preview_level_count,
+};
 use crate::helper::screen::Screen;
 
 use game::model::coordinate::Coordinate;
@@ -23,7 +25,8 @@ pub struct LevelPreviewPageProps {
 
 #[function_component(LevelPreviewPage)]
 pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
-    let generate_nr = 20;
+    let generate_nr = retrieve_preview_level_count();
+    let level_count = use_state(|| generate_nr);
     let generated_levels = (0..generate_nr)
         .into_iter()
         .map(|index| generate(*props.dimension, index as u64))
@@ -31,7 +34,6 @@ pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
 
     let saved_level = retrieve_editor_level();
 
-    let level_count = use_state(|| generate_nr);
     let reducer = use_reducer(PreviewState::set(generated_levels));
 
     let load_more_levels: Callback<MouseEvent> = {
@@ -41,6 +43,7 @@ pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
         Callback::from(move |_| {
             log::info!("loading more levles");
             reducer.dispatch(PreviewAction::LoadNew(10, *dimension));
+            save_preview_level_count(*level_count + 10);
             level_count.set(*level_count + 10);
         })
     };
@@ -54,7 +57,6 @@ pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
             let num = rand::thread_rng().gen_range(0..*level_count);
             let level = reducer.extracted_levels[num].clone();
             change_screen(screen.clone(), Screen::Level(level));
-            //screen.set(Screen::Level(reducer.extracted_levels[num].clone()));
         })
     };
 
@@ -90,7 +92,9 @@ pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
                     <div id="saved-level">
                         <div
                             class="level-container"
-                            onclick={to_level_action(saved_level.clone(), props.screen.clone())}>
+                            onclick={to_level_action(
+                                saved_level.clone(),
+                                props.screen.clone())}>
                             <StatelessLevelComponent level_grid={saved_level.clone()} />
                             <div class="level-title">{"Saved"}</div>
                         </div>
@@ -105,8 +109,8 @@ pub fn level_preview_page_component(props: &LevelPreviewPageProps) -> Html {
                             html!{
                                 <div
                                     class="level-container"
-                                    onclick={
-                                        to_level_action(level_grid.clone(),
+                                    onclick={to_level_action(
+                                        saved_level.clone(),
                                         props.screen.clone())}>
                                     <StatelessLevelComponent level_grid={level_grid.clone()} />
                                     <div class="level-title">
